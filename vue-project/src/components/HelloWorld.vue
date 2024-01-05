c40e74b98033e55e908baa8fc8dcc0ac8d00458c
zhanghed <747988598@qq.com>
2024-01-05 16:14:55 +0800

    w

--- vue-project/src/components/HelloWorld.vue
+++ vue-project/src/components/HelloWorld.vue
@@ -8,6 +8,24 @@ defineProps<{
   <div>
     <h1>{{ msg }}</h1>
   </div>
+  <div class="a">
+    <div class="b"></div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.a {
+  width: 200px;
+  height: 200px;
+  background-color: aquamarine;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.b {
+  width: 100px;
+  height: 100px;
+  background-color: antiquewhite;
+}
+</style>
